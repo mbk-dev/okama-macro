74f354ee0efe47324b4b5a5c4e507bfb9038909a
Sergey Kikevich <sergey@rostsber.ru>
2022-08-16 11:46:05 +0300

    feat: calculate long term monthly inflation 1987-2000

--- inflation.py
+++ inflation.py
@@ -11,7 +11,8 @@ def get_inflation_from_2001() -> pd.Series:
     s_2016 = request_data.load_nbs_web(series='A01030101', periods=f'2016-{today_year}', freq='month')
     s_2001 = request_data.load_nbs_web(series='A01030201', periods='2001-2015', freq='month')
     s = pd.concat([s_2016, s_2001], axis=0, join="outer", copy="false")
-    s -= 100.
+    s = (s - 100.) / 100
+    s.index = s.index.to_period("M")
     return s.sort_index(ascending=False)
 
 
@@ -20,7 +21,9 @@ def get_recent_inflation(first_year: str = '2016') -> pd.Series:
     Request monthly inflation time series (2016+).
     """
     s_2016 = request_data.load_nbs_web(series='A01030101', periods=f'{first_year}-{today_year}', freq='month')
-    return s_2016 - 100.
+    s_2016.index = s_2016.index.to_period("M")
+    s_2016.sort_index(ascending=False, inplace=True)
+    return (s_2016 - 100.) / 100
 
 
 def get_annual_inflation() -> pd.Series:
@@ -30,8 +33,20 @@ def get_annual_inflation() -> pd.Series:
     Inflation is calculated from CPI ('A01010201' code - same month of last year=100).
     """
     s_old = request_data.load_nbs_web(series='A01010201', periods='1987-2015', freq='month')
+    s_old.index = s_old.index.to_period("M")
+    s_old.sort_index(ascending=False, inplace=True)
     s_old = s_old[s_old != 0]
-    return s_old - 100.
+    return (s_old - 100.) / 100
 
 
-
+def calculate_monthly_from_annual():
+    """
+    Calculate monthly inflation (1987 - 2000) from annual and monthly data.
+    """
+    s_annual = get_annual_inflation()
+    s_monthly = get_inflation_from_2001()
+    new_index = pd.period_range('1987-01', '2000-12', freq='M').sort_values(ascending=False)
+    for date in new_index:
+        prod_monthly = (s_monthly.tail(11) + 1.).prod()
+        s_monthly[date] = (s_annual.shift(11)[date] + 1.) / prod_monthly - 1.
+    return s_monthly
