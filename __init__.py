d7229d290c6c7f54168547739c3eaf6ed6623770
Sergey Kikevich <sergey@rostsber.ru>
2022-08-18 11:17:08 +0300

    chore: add main functions to __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version
+
+from nbsc.inflation import get_inflation_from_2001, get_recent_inflation, get_annual_inflation, \
+    calculate_monthly_from_annual
+from nbsc.request_data import load_nbs_web
+
+__version__ = version("nbsc")
