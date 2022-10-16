2497561e0c8ff1e2710bd0481d2104edec8b26d9
fzhulitov <fzhulitov@gmail.com>
2022-10-16 20:08:18 +0300

    Work, but not to like the conversion method, and still can't name colums )

--- request_data.py
+++ request_data.py
@@ -6,7 +6,9 @@ from requests import Response
 
 URL_begin = "https://www.boi.org.il/en/DataAndStatistics/Pages/boi.ashx"
 
-def get_data_frame( seriescode: str = 'RIB_BOI.D', datestart: str = "27/01/1994", dateend: str = '13/10/2022') -> pd.Series:
+
+def get_data_frame(seriescode: str = 'RIB_BOI.D',
+                   datestart: str = "27/01/1994", dateend: str = '13/10/2022') -> pd.Series:
     request_url = URL_begin
     params = {'Command': 'DownloadSeriesExcel',
               'SeriesCode': seriescode,
@@ -16,11 +18,9 @@ def get_data_frame( seriescode: str = 'RIB_BOI.D', datestart: str = "27/01/1994"
               'Sid': '22'
               }
     abc: Response = requests.get(request_url, params=params)
-    jresp = abc.text
-    df = pd.read_html(StringIO(jresp),
-                    header=15,
-                      index_col=0
-                    )
-    df.set_index('TIME_PERIOD', inplace=True)
-    df.index = df.index.to_period(freq= 'D')
+    resp = abc.text
+    ldf = pd.read_html(StringIO(resp), parse_dates=[0])
+    df = ldf[1]
+    df.set_index(0, inplace=True)
+    df.index = df.index.to_period(freq='D')
     return df
