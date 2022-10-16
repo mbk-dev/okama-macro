223d93e92dc7b63fe7403e4ca0815a103cd5cbaf
fzhulitov <fzhulitov@gmail.com>
2022-10-16 20:05:42 +0300

    sintaksys

--- gdp.py
+++ gdp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import ecb
 
+
 def get_gdp_q() -> pd.Series:
     df = ecb.request_data.get_data_frame('MNA', 'Q.N.I8.W2.S1.S1.B.B1GQ._Z._Z._Z.EUR.V.N', 'Q')
     return df
--- kr.py
+++ kr.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import ecb
+
+
 def get_main_rate() -> pd.Series:
     df = ecb.request_data.get_data_frame('FM', 'D.U2.EUR.4F.KR.MRR_FR.LEV')
     return df
+
 
 def get_deposit_rate() -> pd.Series:
     df = ecb.request_data.get_data_frame('FM', 'D.U2.EUR.4F.KR.DFR.LEV')
     return df
 
+
 def get_marginal_rate() -> pd.Series:
     df = ecb.request_data.get_data_frame('FM', 'D.U2.EUR.4F.KR.MLFR.LEV')
     return df
-
