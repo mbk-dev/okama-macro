4e5c0160b9ee3cf4e9286acd4d85127bb0ca661f
fzhulitov <fzhulitov@gmail.com>
2022-10-17 15:32:25 +0300

    All correct and naming

--- kr.py
+++ kr.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import boi
+from datetime import date
 
+today = date.today()
 
-def get_ir(datestart: str = "27/01/1994", dateend: str = '13/10/2022') -> pd.Series:
+def get_ir(datestart: str = "27/01/1994", dateend: str = today.strftime("%d/%m/%Y")) -> pd.Series:
     s = boi.request_data.get_data_frame(seriescode='RIB_BOI.D', datestart=datestart, dateend=dateend)
     s.rename("key_rate", inplace=True)
     return s / 100
