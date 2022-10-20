9aa03464e613dd0fa2dc085e0f064a50e5cf7681
fzhulitov <fzhulitov@gmail.com>
2022-10-20 18:00:29 +0300

    correction of __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
+from importlib.metadata import version
+
+
 from boi.request_data import get_data_frame
 from boi.kr import get_ir
 from boi.gdp import get_gdp
 from boi.infl import get_cp
+
+
+# __version__ = version("boi")
