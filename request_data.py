be78aa84555de01a175cf805eefbbc2d22a24b3c
Sergey Kikevich <sergey@rostsber.ru>
2022-10-21 09:27:24 +0300

    feat: track errors in requests

--- request_data.py
+++ request_data.py
@@ -4,20 +4,28 @@ from io import StringIO
 
 from requests import Response
 
-URL_begin = "https://sdw-wsrest.ecb.europa.eu/service/data/"
-URL_end = "/"
+URL_BASE = "https://sdw-wsrest.ecb.europa.eu/service/data/"
+URL_END = "/"
 
 
 def get_data_frame(agency: str = 'FM', code: str = 'D.U2.EUR.4F.KR.MRR_FR.LEV', freq: str = 'D',
                    detail: str = "dataonly", format: str = "csvdata",
                    startperiod: str = '1900-01-01', endperiod: str = None) -> pd.Series:
-    request_url = URL_begin + agency + URL_end + code
+    request_url = URL_BASE + agency + URL_END + code
     params = {'detail': detail,
               'format': format,
               'startPeriod': startperiod,
               'endPeriod': endperiod
               }
-    abc: Response = requests.get(request_url, params=params)
+    try:
+        abc: Response = requests.get(request_url, params=params)
+    except requests.exceptions.HTTPError as err:
+        raise requests.exceptions.HTTPError(
+            f"HTTP error fetching data for {code}:",
+            abc.status_code,
+            abc.reason,
+            URL_BASE,
+        ) from err
     jresp = abc.text
     df = pd.read_csv(StringIO(jresp),
                      usecols=['TIME_PERIOD', 'OBS_VALUE'],
