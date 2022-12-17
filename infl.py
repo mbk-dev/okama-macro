476e32f81e3183f0bfcd611f9ba3285581bcb1d5
fzhulitov <fzhulitov@gmail.com>
2022-12-17 12:20:33 +0200

    correction of infl - to standart

--- infl.py
+++ infl.py
@@ -8,4 +8,6 @@ today = date.today()
 def get_cp(datestart: str = "01/01/1951", dateend: str = today.strftime("%d/%m/%Y")) -> pd.Series:
     s = boi.request_data.get_data_frame(seriescode='CP', datestart=datestart, dateend=dateend, freq='M')
     s.rename("CP based on 2020", inplace=True)
+    s = s.pct_change().round(4)
+    s.dropna(inplace=True)
     return s
