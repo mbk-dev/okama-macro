45938eba732acf4d73875bc25a5cce887506d3f6
Sergey Kikevich <sergey@rostsber.ru>
2023-03-14 20:53:39 +0300

    fix: change URL for CPI

--- gdp.py
+++ gdp.py
@@ -9,6 +9,8 @@ def get_gdp(datestart: str = "1900-1-1",
             dateend: str = today.strftime(boi.request_data.format_long)) -> pd.Series:
     s = boi.request_data.get_data_frame(url=url,
                                         seriescode='GDP_Q_N.GDP.GDP11.Q.N.V.S1._Z.N_CHN.ILS.N',
-                                        datestart=datestart, dateend=dateend, freq='Q')
+                                        datestart=datestart,
+                                        dateend=dateend,
+                                        freq='Q')
     s.rename("GDP in Mil NIS", inplace=True)
     return s
--- infl.py
+++ infl.py
@@ -5,17 +5,29 @@ from datetime import date
 today = date.today()
 
 
-def get_cp(datestart: str = "1900-1-1",
-           dateend: str = today.strftime(boi.request_data.format_long)) -> pd.Series:
-    s = boi.request_data.get_data_frame(seriescode='CP.CPI.CPI_2_29.MAIN.M.N._Z._Z.I20_L._Z.A._Z',
-                                        datestart=datestart, dateend=dateend, freq='M')
-    s.rename("CP based on 2020", inplace=True)
+def get_cpi(date_start: str = "1900-1-1",
+            date_end: str = today.strftime(boi.request_data.format_long)
+            ) -> pd.Series:
+    """
+    Get Total Consumer Price Index (CPI).
+
+    CPI 2020 is 100%.
+    """
+    url_cpi = "https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/PRI/1.0/"
+    s = boi.request_data.get_data_frame(
+        url=url_cpi,
+        seriescode='CP.CPI.CPI_2_29.MAIN.M.N._Z._Z.I22_L._Z.A._Z._Z',
+        datestart=date_start,
+        dateend=date_end,
+        freq='M')
+    s.rename("CPI_2020", inplace=True)
     return s
 
 
-def get_inflation(datestart: str = "1900-1-1",
-                  dateend: str = today.strftime(boi.request_data.format_long)) -> pd.Series:
-    s = get_cp(datestart, dateend)
+def get_inflation(date_start: str = "1900-1-1",
+                  date_end: str = today.strftime(boi.request_data.format_long)
+                  ) -> pd.Series:
+    s = get_cpi(date_start, date_end)
     s.rename("Inflation", inplace=True)
     s = s.pct_change().round(4)
     s.dropna(inplace=True)
