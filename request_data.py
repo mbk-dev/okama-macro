141ca964f589252a69ebd9c850bb33e7d1ab2197
Sergey Kikevich <sergey@rostsber.ru>
2023-03-23 10:21:35 +0300

    feat: handle requests errors

--- request_data.py
+++ request_data.py
@@ -5,25 +5,42 @@ from datetime import date
 import pandas as pd
 import requests
 from requests import Response
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
-
-format_long = "%Y-%m-%d"
-format_short = '%Y-%m'
+from boi.settings import format_long, format_short
 
 today = date.today()
 
-URL_begin = "https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/PRI/1.0/"
+BASE_URL = "https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/PRI/1.0/"
 
 
-def get_data_frame(url: str = URL_begin,
-                   series_code: str = 'RIB_BOI.D',
-                   date_start: str = "1900-1-1",
-                   date_end: str = today.strftime(format_long),
-                   freq: str = 'D') -> pd.Series:
+def get_data(url: str = BASE_URL,
+             series_code: str = 'RIB_BOI.D',
+             date_start: str = "1900-1-1",
+             date_end: str = today.strftime(format_long),
+             freq: str = 'D') -> pd.Series:
     request_url = url + series_code
     date_parameter = urllib.parse.unquote('c%5BTIME_PERIOD%5D')
     params = {date_parameter: f'ge:{date_start}+le:{date_end}'}
-    abc: Response = requests.get(request_url, params=params)
+    # Prepare the HTTP request
+    session = requests.session()
+    retry_strategy = Retry(
+        total=3, backoff_factor=0.1, status_forcelist=[429, 500, 502, 503, 504]
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session.mount("http://", adapter)
+
+    try:
+        abc: Response = requests.get(request_url, params=params)
+        abc.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise requests.exceptions.HTTPError(
+            f"HTTP error fetching data for {series_code}:",
+            abc.status_code,
+            abc.reason,
+            BASE_URL,
+        ) from err
     resp = abc.text
 
     ldf = pd.read_xml(StringIO(resp), xpath="//Obs")
