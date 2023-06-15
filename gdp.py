5a362e6c7ff5860cecfe9dcd1e403304a08feec3
Sergey Kikevich <sergey@rostsber.ru>
2023-06-15 18:54:57 +0300

    fix: some data returned as DF not Series

--- gdp.py
+++ gdp.py
@@ -9,10 +9,11 @@ url = "https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATIS
 
 def get_gdp(date_start: str = "1900-1-1",
             date_end: str = today.strftime(boi.settings.format_long)) -> pd.Series:
-    s = boi.request_data.get_data(url=url,
+    df = boi.request_data.get_data(url=url,
                                   series_code='GDP_Q_N.GDP.GDP11.Q.N.V.S1._Z.N_CHN.ILS.N',
                                   date_start=date_start,
                                   date_end=date_end,
                                   freq='Q')
+    s = df.iloc[:, 0]  # first column has values
     s.rename("GDP in Mil NIS", inplace=True)
     return s
--- infl.py
+++ infl.py
@@ -19,12 +19,13 @@ def get_cpi(date_start: str = "1951-10-01",
                CP_PCH.CPI.CPI_2_29.MAIN.M.N._Z._Z.PT._Z.A.G1._Z
     """
     url_cpi = "https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/PRI/1.0/"
-    s = boi.request_data.get_data(
+    df = boi.request_data.get_data(
         url=url_cpi,
         series_code='CP.CPI.CPI_2_29.MAIN.M.N._Z._Z.I22_L._Z.A._Z._Z.CP',
         date_start=date_start,
         date_end=date_end,
         freq='M')
+    s = df.iloc[:, 0]  # first column has values
     s.rename("CPI_2020", inplace=True)
     return s
 
